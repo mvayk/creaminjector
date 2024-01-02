f97528848d16607d5de02bd12abbe6cde0ab220f
Nyemia <pressesc@outlook.com>
2024-01-02 15:31:12 +1100

    new file:   a.exe 	new file:   creaminjector_exampledll/exampleDLL/.vs/exampleDLL/FileContentIndex/d4ae46e0-fb7c-4f3e-b528-79a743471dd9.vsidx

--- injector.cpp
+++ injector.cpp
@@ -1,33 +1,39 @@
 #include <iostream>
 #include <windows.h>
-#include <winuser.h>
+#include <TlHelp32.h>
+#include <Shlwapi.h>
 
-DWORD pid;
+int main(int argc, char** argv) { 
 
-DWORD getWindow(LPCSTR argu) {
-  HWND hwnd = FindWindowA(0, (argu));
-  GetWindowThreadProcessId(hwnd, &pid);
-
-  if (hwnd) {
-    std::cout << pid << std::endl;
-    return pid;
+  if (argc != 3) {
+    std::cout << "1: filepath to dll 2: name of process to inject to" << std::endl;
   } else {
-    std::cout << "Couldn't find PID" << std::endl;
+    DWORD pid;
+    std::string sudodllpath = argv[1];
+    LPCSTR processName = argv[2];
+
+    /*
+    if (PathFileExists(dllpath.c_str())) {
+      std::cout << "Nonexistent file path" << std::endl;
+      return EXIT_FAILURE;
+    }
+    */
+
+    HWND hwnd = FindWindowA(0, (processName));
+    GetWindowThreadProcessId(hwnd, &pid);
+    const char* dllpath = argv[1];
+
+    if (pid == NULL) {
+      return EXIT_FAILURE;
+    } else {
+      HANDLE process = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pid);
+      LPVOID alloc = VirtualAllocEx(process, 0, strlen(dllpath) + 1, MEM_COMMIT, PAGE_READWRITE);
+      WriteProcessMemory(process, alloc, dllpath, strlen(dllpath) + 1, 0);
+      HANDLE loadThread = CreateRemoteThread(process, 0, 0, (LPTHREAD_START_ROUTINE)GetProcAddress(GetModuleHandleA("Kernel32.dll"), "LoadLibraryA"), alloc, 0, 0);
+      WaitForSingleObject(process, INFINITE);
+      VirtualFreeEx(process, alloc, strlen(dllpath) + 1, MEM_RELEASE);
+    }
   }
-}
-
-// int main(void* dllpath, LPCSTR processName) {
-int main(char* dllpath, LPCSTR processName) {
-  pid = getWindow(processName);
-
-  std::cout << "takes pid as argument" << std::endl;
-
-  HANDLE process = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pid);
-  LPVOID alloc = VirtualAllocEx(process, 0, strlen(dllpath) + 1, MEM_COMMIT, PAGE_READWRITE);
-  WriteProcessMemory(process, dllpath, (LPVOID)dllpath, strlen(dllpath) + 1, 0);
-  HANDLE loadThread = CreateRemoteThread(process, 0, 0, (LPTHREAD_START_ROUTINE)GetProcAddress(GetModuleHandleA("Kernel32.dll"), "LoadLibraryA"), dllpath, 0, 0);
-  WaitForSingleObject(process, INFINITE);
-  VirtualFreeEx(process, dllpath, strlen(dllpath) + 1, MEM_RELEASE);
 
   return 0;
 }

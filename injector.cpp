1356cfafc6a2f1082ee1cc3bbea8c3e71e555729
Nyemia <pressesc@outlook.com>
2024-01-02 23:59:25 +1100

    new file:   a.exe 	new file:   creaminjector_exampledll/exampleDLL/x64/Release/exampleDLL.dll 	modified:   injector.cpp

--- injector.cpp
+++ injector.cpp
@@ -3,17 +3,33 @@
 #include <windows.h>
 #include <TlHelp32.h>
 #include <Shlwapi.h>
+#include <string>
+#include <sys/stat.h>
+
+// TODO: check if dllpath exists and add different types of injection + better command syntax
+
+inline bool fileExists(const std::string &path) {
+  struct stat buffer;
+  return(stat(path.c_str(), &buffer) == 0);
+}
 
 int main(int argc, char** argv) { 
   if (argc != 3) {
-    std::cout << "1: filepath to dll 2: name of process to inject to" << std::endl;
+    std::cout << "[CreamInjector] > ARG1: Path to DLL C:\\class.dll ARG2: Name of Process Untitled - Notepad" << std::endl;
   } else {
     DWORD pid;
     LPCSTR processName = argv[2];
     HWND hwnd = FindWindowA(0, (processName));
     GetWindowThreadProcessId(hwnd, &pid);
     const char* dllpath = argv[1];
-    if (pid == NULL) {
+
+    if (!fileExists(dllpath)) {
+      std::cout << "[CreamInjector] > Couldn't find path." << std::endl;
+      return EXIT_FAILURE;
+    }
+
+    if (!hwnd) {
+      std::cout << "[CreamInjector] > Couldn't find PID from Process Name." << std::endl;
       return EXIT_FAILURE;
     } else {
       HANDLE process = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pid);
@@ -22,6 +38,10 @@ int main(int argc, char** argv) {
       HANDLE loadThread = CreateRemoteThread(process, 0, 0, (LPTHREAD_START_ROUTINE)GetProcAddress(GetModuleHandleA("Kernel32.dll"), "LoadLibraryA"), alloc, 0, 0);
       WaitForSingleObject(process, INFINITE);
       VirtualFreeEx(process, alloc, strlen(dllpath) + 1, MEM_RELEASE);
+      if (process) {
+        CloseHandle(process);
+        std::cout << "[CreamInjector] > Sucessfully closed handle" << std::endl;
+      }
       return EXIT_SUCCESS;
     }
   }
